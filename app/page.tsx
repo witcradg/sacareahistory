ad44a4f887f287806e8aff9d3067ec6c361e18d6
witcradg <witcradg@gmail.com>
2024-10-08 13:44:40 -0700

    Example timeline on home page

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
         </div>
       </Card> */}
       <Hero />
-      <section className="w-[80vw] m-auto py-4 my-4 bg-card">
+      <section className="w-[80vw] m-auto py-4 mt-4 mb-16 bg-card">
         <div>
           <h2 className="w-full block text-2xl font-bold text-center">Resources &ndash; Alphabetical</h2>
         </div>
@@ -89,6 +89,41 @@ export default function Home() {
           </TabsContent>
         </Tabs>
       </section>
+
+      <div className="w-1/2 my-12 mx-auto">
+        <h2 className="text-2xl font-bold text-center">Timeline</h2>
+        <ol className="relative border-s border-black dark:border-white">
+          <li className="mb-10 ms-4">
+            <div className="absolute w-3 h-3 bg-gray-900 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-900 dark:text-gray-500">
+              February 1895</time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              Timeline Event One</h3>
+            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+              This is a descrption of Timeline Event One</p>
+            <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+              Additional Information <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+              </svg></a>
+          </li>
+          <li className="mb-10 ms-4">
+            <div className="absolute w-3 h-3 bg-gray-900 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-900 dark:text-gray-500">
+              March 1895</time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Founding of ... </h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">A description of the above item</p>
+          </li>
+          <li className="ms-4">
+            <div className="absolute w-3 h-3 bg-gray-900 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-900 dark:text-gray-200">April 1985</time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Another Event</h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">A description of the above item</p>
+          </li>
+        </ol>
+
+
+      </div>
+
     </>
   );
 }
