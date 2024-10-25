06c5ac0e9018b263b3e94dd961bf22e2f0921c8c
witcradg <witcradg@gmail.com>
2024-10-25 12:53:15 -0700

    Add placeholder page for calendar

---
+++ app/calendar/page.tsx
@@ -0,0 +1,15 @@
+import { Hero } from "@/components/Hero"
+import { ResourcesSection } from "@/components/ResourcesSection"
+import { TimelineSection } from "@/components/timeline/TimelineSection";
+// import ColorPalette from "@/components/ColorPalette";
+
+
+export default function Home() {
+  return (
+    <div className="h-svh bg-">
+      {/* <ColorPalette /> */}
+      CALEDAR PLACEHOLDER
+    </div>
+  );
+}
+
