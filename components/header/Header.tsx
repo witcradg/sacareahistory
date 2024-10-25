6fb93d1fcbeb281dca79c6d024e2e46c22042615
witcradg <witcradg@gmail.com>
2024-10-25 12:52:58 -0700

    Add desktop menu

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Image from "next/image";
 import Link from "next/link";
+import { MobileMenu } from "./MobileMenu";
 
 import {
   NavigationMenu,
@@ -10,12 +11,16 @@ import {
 
 import { ModeToggle } from "../mode-toggle";
 import CallButton from "../CallButton";
+// import Register from "@/components/Register";
+import { Navbar } from "./Navbar";
+// import { MobileMenu } from "./MobileMenu";
 
 export const Header = () => {
   return (
+
     <header className="sticky border-b-[1px] top-0 z-40 w-full bg-card dark:border-b-slate-700 dark:bg-background">
       <NavigationMenu className="mx-auto">
-        <NavigationMenuList className="container h-15 px-4 w-screen flex justify-between ">
+        <NavigationMenuList className="mx-10 h-15 px-24 w-screen flex justify-between ">
           <NavigationMenuItem className="font-bold flex">
             <Link
               rel="noreferrer noopener"
@@ -24,22 +29,22 @@ export const Header = () => {
             >
               <Image className='border-2 my-2'
                 // src="/us-cascr64.gif" width={300} height={69} alt="Logo" />
-                src='/layout/logo.jpg' width={160} height={160} alt='logo' />
+                src='/layout/logo.jpg' width={150} height={150} alt='logo' />
             </Link>
           </NavigationMenuItem>
-
-          {/* <MobileMenu /> */}
-
-          {/* desktop */}
-          {/* <nav className="hidden md:flex gap-2">
-            <Navbar />
-          </nav> */}
 
           <div className="flex flex-col items-start md:justify-center md:items-center">
             <h1 className="text-xl md:text-4xl text-foreground">SacArea History Consortium&apos;s</h1>
             <h2 className="text-base md:text-2xl">Research Reference Info</h2>
+            {/* desktop */}
+            <nav className="hidden md:flex gap-2">
+              <Navbar />
+            </nav>
           </div>
-          <div className="hidden md:flex gap-4">
+          <MobileMenu />
+
+
+          <div className="hidden md:flex gap-2">
             <CallButton />
             <ModeToggle />
           </div>
