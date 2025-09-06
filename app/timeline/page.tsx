0392fa2a9ecfa67bcb7138a51024e46cc6e6e5be
witcradg <witcradg@gmail.com>
2025-09-06 13:15:29 -0700

    WIP test docx

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -3,7 +3,24 @@
 import { TimelineSection } from "@/components/timeline/TimelineSection";
 
 export default function TimelinePage() {
+	const domain = process.env.NEXT_PUBLIC_BASE_URL;
+	if (!domain) {
+		return (
+			<div>
+				Error: NEXT_PUBLIC_BASE_URL is not defined.
+			</div>
+		);
+	}
+	const document = `${domain}/timeline.docx`;
+	// const pdfDocument = `${domain}/timeline.pdf`;
 	return (
-		<TimelineSection />
-	)
+		<>
+			<iframe
+				src={`https://docs.google.com/gview?url=${document}&embedded=true`}
+				width="350"
+				height="500"
+			/>
+			<TimelineSection />
+		</>
+	);
 }
