eebe2ac69d0664022beb1e9398aaec03330b5547
witcradg <witcradg@gmail.com>
2025-12-04 09:58:34 -0800

    Refactor CallButton component and remove unused Partners component

--- components/CallButton.tsx
+++ components/CallButton.tsx
@@ -1,32 +1,13 @@
 "use client"
 
-import { Button } from '@/components/ui/button';
-import { useCallback } from 'react';
+import { Button } from "@/components/ui/button"
 
 const CallButton = () => {
-	// const handleCall = useCallback(() => {
-	// 	window.location.href = 'tel:916-794-9078';
-	// }, []);
+  return (
+    <Button asChild className="text-white" aria-label="Call SacArea History Consortium">
+      <a href="tel:+19167949078">Contact Us</a>
+    </Button>
+  )
+}
 
-	return (
-		<Button className='text-white'> CONTACT US
-			{/* <span className="whitespace-nowrap">
-				<svg
-					stroke="currentColor"
-					fill="currentColor"
-					strokeWidth="0"
-					viewBox="0 0 512 512"
-					className="inline m-2 ml-0"
-					height="20px"
-					width="20px"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"></path>
-				</svg>
-				916-794-9078
-			</span> */}
-		</Button>
-	);
-};
-
-export default CallButton;
+export default CallButton
